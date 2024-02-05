1f11a4d1b4a1dabe1e9cc4ed8691ff5968f68fa3
aby <anti1346@gmail.com>
2024-02-05 20:57:45 +0900

    no message

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = 3000;
 app.set('trust proxy', true); // 프록시 신뢰 설정
 
 app.get('/', (req, res) => {
+  // 내부 IP 가져오기
   const internalIP = os.networkInterfaces().eth0[0].address;
   
   // ifconfig.io를 사용하여 외부 IP 가져오기
@@ -19,9 +20,21 @@ app.get('/', (req, res) => {
       return;
     }
     const externalIP = stdout.trim();
-    const hostname = os.hostname(); // 호스트명 가져오기
-    const currentTime = new Date().toLocaleString(); // 현재 시간 가져오기
-    res.send(`Hostname: ${hostname}\nInternal IP: ${internalIP}\nExternal IP: ${externalIP}\nCurrent Time: ${currentTime}\n`);
+
+    // 호스트명 가져오기
+    const hostname = os.hostname(); 
+
+    // 현재 시간을 YYYY.MM.DD HH:MM:SS 형식으로 포맷팅
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+    const formattedDate = `${year}.${month}.${day} ${hours}:${minutes}:${seconds}`;
+
+    res.send(`Hostname: ${hostname}\nInternal IP: ${internalIP}\nExternal IP: ${externalIP}\nCurrent Time: ${formattedDate}\n`);
   });
 });
 
